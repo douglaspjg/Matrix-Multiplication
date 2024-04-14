d6c3bfa36c9b8827bc32b1d556dbf76968d6c18c
Douglas Pineda Gutierrez <djp5@williams.edu>
2024-04-14 11:25:02 -0400

    Initial Commit

---
+++ Matrix Multiplication/Matrix_MultiplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Matrix_MultiplicationApp.swift
+//  Matrix Multiplication
+//
+//  Created by Douglas Pineda-Gutierrez on 4/14/24.
+//
+
+import SwiftUI
+
+@main
+struct Matrix_MultiplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
